3ff786c2085670d885956f018ec8838fac4559a3
Vishnu Prasad <prasadvishnu788@gmail.com>
2026-03-07 19:50:53 +0530

    Completed payment successful and failure component. Changed the ticket component. Converted the field so it can pass thorugh angular pipe and dispaly the date. Backend was modified to send ISO formatted start and end time of the events.

--- src/app/core/models/ticket.model.ts
+++ src/app/core/models/ticket.model.ts
@@ -4,8 +4,8 @@ export interface TicketResponse {
   seats: number[];   // array of seat IDs (currently)
   username: string;
   event_name: string;
-  event_starts: string;  // "HH:mm:ss"
-  event_ends: string;    // "HH:mm:ss"
+  event_starts: string;
+  event_ends: string;
   issued_at: string;     // ISO datetime
   expires_at: string;    // ISO datetime
 }
