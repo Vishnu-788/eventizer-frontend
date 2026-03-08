0f12a1cf7a503ac0ac99fdeec581a836ec20de0b
Vishnu Prasad <prasadvishnu788@gmail.com>
2026-03-08 17:14:43 +0530

    Updated Event list component. Refactored the UI with icons for a fresh intuitive look.

--- src/app/features/shared/event-card-component/event-card-component.ts
+++ src/app/features/shared/event-card-component/event-card-component.ts
@@ -1,11 +1,20 @@
 import {Component, inject, input} from '@angular/core';
 import {CityEvent} from '../../../core/models/event.model';
 import {Router} from '@angular/router';
+import {DatePipe, NgOptimizedImage} from '@angular/common';
+import {DateIcon} from '../icons/date-icon/date-icon';
+import {TimeIcon} from '../icons/time-icon/time-icon';
+import {LocationIcon} from '../icons/location-icon/location-icon';
 
 
 @Component({
   selector: 'app-event-card-component',
   imports: [
+    DatePipe,
+    NgOptimizedImage,
+    DateIcon,
+    TimeIcon,
+    LocationIcon
 
   ],
   templateUrl: './event-card-component.html',
