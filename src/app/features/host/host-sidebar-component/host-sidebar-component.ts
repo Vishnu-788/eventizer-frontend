440090f017a1c6baaf91320e02b5f96d78774ab6
Vishnu Prasad <prasadvishnu788@gmail.com>
2026-03-09 10:14:08 +0530

    Updated the host side-bar, Added the icons to the side bar items.

--- src/app/features/host/host-sidebar-component/host-sidebar-component.ts
+++ src/app/features/host/host-sidebar-component/host-sidebar-component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import {LogoutButton} from "../../shared/logout-button/logout-button";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {DateIcon} from '../../shared/icons/date-icon/date-icon';
+import {AnalyticsIcon} from '../../shared/icons/analytics-icon/analytics-icon';
+import {BookingIcon} from '../../shared/icons/booking-icon/booking-icon';
+import {ProfileIcon} from '../../shared/icons/profile-icon/profile-icon';
 
 @Component({
   selector: 'app-host-sidebar-component',
   imports: [
     LogoutButton,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    DateIcon,
+    AnalyticsIcon,
+    BookingIcon,
+    ProfileIcon
   ],
   templateUrl: './host-sidebar-component.html',
   styleUrl: './host-sidebar-component.scss',
